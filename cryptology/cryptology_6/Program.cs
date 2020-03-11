f3c9d4fcdf64ce73603d1975fc2d875691500102
ksenyaklakovych <kklakovych@gmail.com>
2020-03-11 09:33:50 +0200

    Added project files

---
+++ cryptology/cryptology_6/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptology_6
+{
+    class A
+    {
+        private long p, a;
+        public long x;
+        public A(long _p, long _a, long _x = 7)
+        {
+            p = _p;
+            a = _a;
+            x = _x;
+        }
+
+        public long CalculateX()
+        {
+            return (long)(Math.Pow(a, x) % p);
+        }
+
+        public long CalculateK(long YFromB)
+        {
+            return (long)(Math.Pow(YFromB, x) % p);
+        }
+    }
+
+    class B
+    {
+        private long p, a;
+        public long y;
+        public B(long _p, long _a, long _y = 4)
+        {
+            p = _p;
+            a = _a;
+            y = _y;
+        }
+
+        public long CalculateY()
+        {
+            return (long)(Math.Pow(a, y) % p);
+        }
+
+        public long CalculateK(long XFromA)
+        {
+            return (long)(Math.Pow(XFromA, y) % p);
+        }
+    }
+
+    class DiffieHellman
+    {
+        public static bool IsPrimeNumber(long p)
+        {
+            for (long i = 2; i < p; ++i)
+            {
+                if (p % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static long PowerMod(long x, long y, long p)
+        {
+            long res = 1;
+            x = x % p;
+            while (y > 0)
+            {
+                if (y % 2 != 0)
+                {
+                    res = (res * x) % p;
+                }
+                y = y >> 1;
+                x = (x * x) % p;
+            }
+            return res;
+        }
+
+        public static long FindPrimitiveRoot(long p)
+        {
+            List<long> fact = new List<long>();
+            long phi = p - 1, n = phi;
+            for (long i = 2; i * i <= n; ++i)
+            {
+                if (n % i == 0)
+                {
+                    fact.Add(i);
+                    while (n % i == 0)
+                    {
+                        n /= i;
+                    }
+                }
+            }
+            if (n > 1)
+            {
+                fact.Add(n);
+            }
+
+            for (long res = 2; res <= p; ++res)
+            {
+                bool ok = true;
+                for (int i = 0; i < fact.Count && ok; ++i)
+                {
+                    ok &= PowerMod(res, phi / fact[i], p) != 1;
+                }
+                if (ok)
+                {
+                    return res;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            
+                    Console.Write("Input p=");
+                    long p = Convert.ToInt64(Console.ReadLine());
+                    if (!IsPrimeNumber(p))
+                    {
+                        throw new ArgumentException("P must be prime number!");
+                    }
+                    long a = FindPrimitiveRoot(p);
+                    Console.WriteLine("g: " + a);
+                    Random rand = new Random();
+                    A first = new A(p, a, rand.Next(8));
+                    B second = new B(p, a, rand.Next(8));
+                    Console.WriteLine("\na: {0}  b:{1}", first.x, second.y);
+                    long xfroma = first.CalculateX();
+                    long yfromb = second.CalculateY();
+                    Console.WriteLine("\nA: " + xfroma);
+                    Console.WriteLine("B: " + yfromb);
+                    long kfroma = first.CalculateK(yfromb);
+                    long kfromb = second.CalculateK(xfroma);
+                    Console.WriteLine("s from A: " + kfroma);
+                    Console.WriteLine("s from B: " + kfromb);
+                    Console.WriteLine("s from A & s from B " + ((kfroma == kfromb) ? "EQUALS!!!" : "NOT equals!!!"));
+                    Console.ReadKey();
+        }
+    }
+}
+
